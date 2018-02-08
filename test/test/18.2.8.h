59045d0966ef02a753e835541ab8610b519a8b88
SilkSilkSilk <2461220952@qq.com>
2018-02-08 22:41:23 +0800

    没有找到代码不补全的原因
    模板类中定义的结构体智能指针不会代码补全

---
+++ test/test/18.2.8.h
@@ -0,0 +1,46 @@
+﻿#ifndef __MAIN_18_2_8_H__
+#define __MAIN_18_2_8_H__
+
+#include <iostream>
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+
+template <typename T> class class_18_2_8
+{			//没有找到代码不补全的原因
+public:
+	struct SNode;
+
+	using Stack =  shared_ptr<SNode>;
+
+	struct SNode
+	{
+		T Data;
+		Stack Next;
+	};
+	
+	Stack Snode_ptr; //链表的头
+
+	class_18_2_8<T>()
+	{
+		Snode_ptr = make_shared<SNode>();
+		Snode_ptr->Data = 0;
+	}
+	~class_18_2_8<T>()
+	{
+
+	}
+
+	void test(T a)
+	{
+		
+	}
+};
+
+
+
+
+
+#endif
--- test/test/main.cpp
+++ test/test/main.cpp
@@ -1,11 +1,21 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 
+#include "18.2.8.h"
 
 using namespace std;
 
+template<typename T>			//模板类的别名方法
+using aa = class_18_2_8<T>;
+
 int main()
 {
+	class_18_2_8<int> p();
+
+	class_18_2_8<int>::Stack ptr(new class_18_2_8<int>::SNode());
+	ptr->Data = 0;
+
+	aa<int> a;
 
 
 	return 0;

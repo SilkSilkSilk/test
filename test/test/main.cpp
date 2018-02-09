d54d5a1e57796b81024650d7cf7f31fd2cdc797c
SilkSilkSilk <2461220952@qq.com>
2018-02-09 23:19:57 +0800

    运算符类型

--- test/test/main.cpp
+++ test/test/main.cpp
@@ -5,17 +5,10 @@
 
 using namespace std;
 
-template<typename T>			//模板类的别名方法
-using aa = class_18_2_8<T>;
 
 int main()
 {
-	class_18_2_8<int> p();
-
-	class_18_2_8<int>::Stack ptr(new class_18_2_8<int>::SNode());
-	ptr->Data = 0;
-
-	aa<int> a;
+	decltype(+) a;
 
 
 	return 0;
